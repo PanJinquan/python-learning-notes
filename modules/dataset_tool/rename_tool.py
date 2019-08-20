a426d59a6257b55caf2a3475b2a6a2276c1c905c
panjinquan <pan_jinquan@163.com>
2019-08-20 14:02:13 +0800

    modify rename

--- modules/dataset_tool/rename_tool.py
+++ modules/dataset_tool/rename_tool.py
@@ -7,6 +7,7 @@
     @Date   : 2019-08-09 18:14:37
 """
 import os
+import shutil
 import os.path
 from utils import file_processing
 
@@ -27,10 +28,40 @@ def rename_image_dir(image_list, prefix="ID_"):
         print(image_path)
         print(newName)
         os.rename(image_path, newpath)
+def sys_rename_image_dir(src_dir1,src_dir2,dest_dir):
+    image_list,image_id = file_processing.get_files_labels(src_dir1, postfix=['*.jpg'])
+    class_set =list(set(image_id))
+    class_set.sort()
+    print(class_set)
+    for cls_name in class_set:
+        id=class_set.index(cls_name)+1
+        s1=os.path.join(src_dir1,str(cls_name))
+        s2=os.path.join(src_dir2,str(cls_name))
+        if not os.path.exists(s1):
+            print("no s1 dir:{}".format(s1))
+            continue
+        if not os.path.exists(s2):
+            print("no s2 dir:{}".format(s2))
+            continue
+        d1=file_processing.create_dir(dest_dir,'val')
+        # shutil.copytree(s1, os.path.join(d1,str(id)+"_{}".format(cls_name)))
+        shutil.copytree(s1, os.path.join(d1,str(id)))
+
+
+        d2=file_processing.create_dir(dest_dir,'facebank')
+        # shutil.copytree(s2, os.path.join(d2,str(id)+"_{}".format(cls_name)))
+        shutil.copytree(s2, os.path.join(d2,str(id)))
+
 
 
 if __name__ == '__main__':
     # dir = '/media/dm/dm/project/dataset/face_recognition/NVR/face/NVRS/trainval'
-    dataset_dir = "/media/dm/dm/project/dataset/face_recognition/X4/X4_Face20/"
+    # dataset_dir='F:/clear_data_bzl/val'
+    dataset_dir='F:/clear_data_bzl/facebank'
     image_list = file_processing.get_files_list(dataset_dir, postfix=['*.jpg'])
-    rename_image_dir(image_list)
+    rename_image_dir(image_list, prefix="ID_")
+
+    # src_dir1='F:/clear_data_bzl/val'
+    # src_dir2='F:/clear_data_bzl/facebank'
+    # dest_dir='F:/clear_data_bzl/dest'
+    # sys_rename_image_dir(src_dir1,src_dir2,dest_dir)
